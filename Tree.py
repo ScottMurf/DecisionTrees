7d35dff991932fd145efc5333c957a04311febb5
ScottMurf <furbo1999@rocketmail.com>
2021-11-03 13:46:01 +0000

    Final edits/comments

--- Tree.py
+++ Tree.py
@@ -4,9 +4,13 @@ import os
 
 #Change working directory to current directory
 os.chdir(r"C:\Users\35385\Documents\GitHub\Decision Trees")
+#Format dataframe
 X=format_dataframe("wildfires.csv","yes")
+#Train/Test Splitting
 X1,Test=training_test_split(X,0.6666)
+#Create the Tree
 Tree1=Tree(X1,1,6,False,"ent")
+#Print the accuracy
 print("{}%".format(round(accuracy(Test,Tree1),2)))
+#Print the Tree structure
 printTree(Tree1)
-os.chdir(r"C:\Users\35385\Documents\GitHub\Decision Trees")
