65ea4f7b90a35855576efd557c8c69831930f0a1
ScottMurf <furbo1999@rocketmail.com>
2021-11-03 14:23:55 +0000

    Formatting

--- Tree.py
+++ Tree.py
@@ -1,16 +1,16 @@
-from DecisionTrees import Tree,format_dataframe,training_test_split,accuracy,printTree
-import pandas as pd
+from DecisionTrees import (Tree, format_dataframe, training_test_split,
+accuracy, printTree)
 import os
 
-#Change working directory to current directory
+# Change working directory to current directory
 os.chdir(r"C:\Users\35385\Documents\GitHub\Decision Trees")
-#Format dataframe
-X=format_dataframe("wildfires.csv","yes")
-#Train/Test Splitting
-X1,Test=training_test_split(X,0.6666)
-#Create the Tree
-Tree1=Tree(X1,1,6,False,"ent")
-#Print the accuracy
-print("{}%".format(round(accuracy(Test,Tree1),2)))
-#Print the Tree structure
+# Format dataframe
+X = format_dataframe("wildfires.csv", "yes")
+# Train/Test Splitting
+X1, Test = training_test_split(X, 0.6666)
+# Create the Tree
+Tree1 = Tree(X1, 1, 6, False, "ent")
+# Print the accuracy
+print("{}%".format(round(accuracy(Test, Tree1), 2)))
+# Print the Tree structure
 printTree(Tree1)
